7959aefebf6a95856e26fd22a6528e7db0bd0a5c
BrianSchipper <schipper.brian.02@gmail.com>
2017-02-10 11:56:12 -0300

    Add files via upload

---
+++ ListaCircular.h
@@ -0,0 +1,60 @@
+﻿struct ListElement
+{
+	// NODO, 2 campos, int y next el siguiente
+	int element;
+	ListElement *next;
+};
+class CircularList 
+{
+public:
+	CircularList();// constructor
+	~CircularList(); // destructor
+	ListElement *pushfront(int elemVal);
+	ListElement *pushback(int elemVal);
+
+private:
+	ListElement * root;
+	ListElement * last;
+	int element;
+	
+	
+	CircularList::CircularList()
+	{
+		root->element = NULL;
+	};
+
+
+	CircularList::~CircularList()
+	{
+		delete[] element;
+	};
+
+
+	ListElement*pushFront(ListElement*&elemVal)
+	{
+	if (root->element =NULL)
+		ListElement*newElement = new ListElement();
+		this -> newElement= newElement;
+	else 
+		
+
+	private:
+		int newelement;
+	}
+
+
+	ListElement*pushBack(ListElement*&elemVal)
+	{
+		
+	}
+
+};
+void erase()
+{
+	
+}
+
+int main() 
+{
+	
+}
